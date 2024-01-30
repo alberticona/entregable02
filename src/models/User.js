50b18ea4740ceae93c0cfcc0d7e84a6d9bffe735
alberticona <albert.ticona@ucsp.edu.pe>
2024-01-29 22:05:38 -0500

    first commit

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection'); 
+
+const User = sequelize.define('user', {
+
+    // id: {
+    //     type: DataTypes.SERIAL,
+    //     allowNull: false,
+    //     unique: true
+    // },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+});
+
+module.exports = User;
